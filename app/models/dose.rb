313e3001cd637896520a04ff35c992a8ba551540
Lionel Rowe <lionel.rowe@gmail.com>
2018-11-15 11:57:23 +0800

    add validations for dose

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates :qty, presence: true
+  validates :qty_unit, presence: true,
+  validates_inclusion_of :qty_unit, in: %w[ml unit g oz]
 end
