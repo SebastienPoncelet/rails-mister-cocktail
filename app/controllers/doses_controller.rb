fa3583d46b7afc64f49563a52d711edd8b33a88f
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2018-11-15 17:39:19 +0800

    Doses controller modified

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,11 @@
 class DosesController < ApplicationController
   def create
-    @
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new # keeps user input to avoid writing everything again when error
+    end
   end
 
   def update
@@ -9,5 +14,12 @@ class DosesController < ApplicationController
 
   def destroy
     @
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:qty, :qty_unit, :cocktail, :ingredient)
+
   end
 end
