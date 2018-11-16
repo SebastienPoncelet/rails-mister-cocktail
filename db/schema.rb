caa48653c4d27048952277fb4d6d9df8b5cd4ecb
Sebastien Poncelet <sebastienponcelet@hotmail.com>
2018-11-16 12:22:01 +0800

    Added the photo uploader in the Cocktail model and migrated AddPhotoToCocktails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_074644) do
+ActiveRecord::Schema.define(version: 2018_11_16_041955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2018_11_15_074644) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "pic"
+    t.string "photo"
   end
 
   create_table "doses", force: :cascade do |t|
